2e175e55a66fb3257ad762607fd6f7422fab8f7a
Mohammad Azimifar PC <mmazimifar7@gmail.com>
2025-01-03 22:18:45 +0330

    Changed the locations of orders model

--- obj/Debug/net8.0/MainApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MainApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea7141adce27366aebc654d81bbd81fa1d74207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627f54872e12e1b78a070cb81fc2a8bba55d7472")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
